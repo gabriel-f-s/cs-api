86eced618db6b4182392dd7c22b9ef3a2774aef2
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-19 14:53:25 -0300

    refactor: renaming the CsApiApplication startup class to Startup.

--- src/main/java/com/dev/cs_api/Startup.java
+++ src/main/java/com/dev/cs_api/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsApiApplication {
+public class Startup {
 
     public static void main(String[] args) {
-        SpringApplication.run(CsApiApplication.class, args);
+        SpringApplication.run(Startup.class, args);
     }
 
 }
--- src/test/java/com/dev/cs_api/TestCsApiApplication.java
+++ src/test/java/com/dev/cs_api/TestCsApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestCsApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(CsApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(Startup::main).with(TestcontainersConfiguration.class).run(args);
     }
 
 }
