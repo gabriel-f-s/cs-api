11d2d2dcc01966e4a47febbb149d97c0c2cab06d
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-22 15:27:09 -0300

    feat(identity): identity configuration class where JPA auditing and configuration properties are enabled

---
+++ src/main/java/com/dev/cs_api/identity/configs/IdentityConfig.java
@@ -0,0 +1,11 @@
+package com.dev.cs_api.identity.configs;
+
+import com.dev.cs_api.identity.dtos.RsaKeyProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@EnableJpaAuditing
+public class IdentityConfig { }
