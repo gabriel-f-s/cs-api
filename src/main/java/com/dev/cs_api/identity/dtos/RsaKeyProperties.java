2d08b8116217f0840c9c3b1a353cc38a0c2a9fa5
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-06-22 15:21:03 -0300

    feat(identity): dto for use with RSA keys

---
+++ src/main/java/com/dev/cs_api/identity/dtos/RsaKeyProperties.java
@@ -0,0 +1,14 @@
+package com.dev.cs_api.identity.dtos;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(
+        RSAPublicKey publicKey,
+        RSAPrivateKey privateKey
+) {
+}
+
