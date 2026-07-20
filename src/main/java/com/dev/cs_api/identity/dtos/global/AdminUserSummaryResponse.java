7029bfdca2fa3a50889dfd9eea26e5c34da50571
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-07-20 14:00:33 -0300

    feat(identity): renaming UserSummaryResponse.java to AdminUserSummaryResponse.java

---
+++ src/main/java/com/dev/cs_api/identity/dtos/global/AdminUserSummaryResponse.java
@@ -0,0 +1,38 @@
+package com.dev.cs_api.identity.dtos.global;
+
+import com.dev.cs_api.identity.models.Admin;
+import com.dev.cs_api.identity.models.User;
+
+public record AdminUserSummaryResponse(
+        String id,
+        String name,
+        String email,
+        String role,
+        String status,
+        String createdAt,
+        Boolean forcePasswordChange
+) {
+    public AdminUserSummaryResponse(User user) {
+        this(
+                user.getId().toString(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole().getName().toString(),
+                user.getStatus().toString(),
+                user.getCreatedAt().toString(),
+                user.getForcePasswordChange()
+        );
+    }
+
+    public AdminUserSummaryResponse(Admin admin) {
+        this(
+                admin.getId().toString(),
+                admin.getName(),
+                admin.getEmail(),
+                admin.getRole().getName().toString(),
+                admin.getStatus().toString(),
+                admin.getCreatedAt().toString(),
+                admin.getForcePasswordChange()
+        );
+    }
+}
