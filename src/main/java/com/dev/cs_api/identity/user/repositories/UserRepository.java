ae78da76c5607c953d667babc4304aea9c24a13c
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-07-22 15:46:19 -0300

    feat(identity): adding findAllWithTenantId query method

--- src/main/java/com/dev/cs_api/identity/user/repositories/UserRepository.java
+++ src/main/java/com/dev/cs_api/identity/user/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.dev.cs_api.identity.user.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,6 +16,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByIdAndTenantId(UUID id, UUID tenantId);
     Page<User> findAllByTenantId(UUID tenantId, Pageable pageable);
+
+    @Query("select u from User u where u.tenantId is not null")
+    Page<User> findAllWithTenantId(Pageable pageable);
 
     void deleteByIdAndTenantId(UUID userId,  UUID tenantId);
 }
