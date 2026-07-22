66b713714ffa1c5e97a05240cf933ab18fe79af5
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-07-22 15:45:55 -0300

    feat(identity): adding @Email validation in UserCreateRequest

--- src/main/java/com/dev/cs_api/identity/user/dtos/UserCreateRequest.java
+++ src/main/java/com/dev/cs_api/identity/user/dtos/UserCreateRequest.java
@@ -1,6 +1,7 @@
 package com.dev.cs_api.identity.user.dtos;
 
 import com.dev.cs_api.identity.user.enums.RoleName;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -8,6 +9,7 @@ public record UserCreateRequest(
         @NotBlank(message = "Nome é obrigatório")
         String name,
         @NotBlank(message = "E-mail é obrigatório")
+        @Email(message = "Email não é válido")
         String email,
         @NotBlank(message = "Senha é obrigatório")
         @Size(min = 8, max = 64, message = "A senha deve ter entre 8 e 64 caracteres")
